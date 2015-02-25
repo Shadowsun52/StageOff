cb8511b9c87211aaa470b003a0b4aa27045609ea
Alexandre <jenicot.alexandre@gmail.com>
2015-02-25 14:30:03 +0100

    Ajout des getters et setters pour la classe Document

--- business/Document.class.php
+++ business/Document.class.php
@@ -1,4 +1,5 @@
 <?php
+namespace stageOff\business;
 
 /**
  * Description of Document
@@ -6,36 +7,50 @@
  * @author Alexandre
  */
 abstract class Document {
+    const FIRST_LINE = 7;
+    /**
+     * @var PHPExcel objet document excel 
+     */
     private $_excel_doc;
+    
+    /**
+     * @var int Ligne courante dans le document excel 
+     */
     private $_current_line;
+    
+    /**
+     * @var Stage stage lier au document 
+     */
     private $_Stage;
+    
+    /**
+     * @var DatabaseAccess 
+     */
     private $_db_access;
     
+    /**
+     * 
+     * @param int $id_questionnaire identifiant du questionnaire
+     * @param int $id_stage identifiant du stage 
+     */
     public function __construct($id_questionnaire, $id_stage) {
         $this->setDbAccess();
         $this->initExcelDoc();
         $this->createSheet($id_questionnaire);
     }
     
-    protected function getExcelDoc() {
-        return $this->_excel_doc;
-    }
-    
-    protected function setExcelDoc($excel_doc) {
-        $this->_excel_doc = $excel_doc;
-    }
-    
     protected function initExcelDoc() {
         $this->setExcelDoc(new PHPExcel());
     }
-
-
-    protected function getCurrentSheet() {
-        return $this->getExcelDoc()->getActiveSheet();
-    }
     
+    /**
+     * 
+     * @param int $id_questionnaire identifiant du questionnaire
+     * @param int $index_sheet index de la nouvelle feuille excel
+     * @throws Exception
+     */
     protected function createSheet($id_questionnaire, $index_sheet=0) {
-        if(empty($id_questionnnaire))
+        if(empty($id_questionnaire))
         {
             throw new Exception("Aucun questionnaire selectionné");
         }
@@ -44,19 +59,86 @@ abstract class Document {
         $this->getCurrentSheet()->setTitle(
                 $this->getDbAccess()->getQuestionnaireTitle($id_questionnaire));
     }
+    
+//<editor-fold defaultstate="collapsed" desc="writer">
+    protected function writeLogo() {
+        
+    }
+//</editor-fold>
 
+//<editor-fold defaultstate="collapsed" desc="getter&setter">
+    /**
+     * 
+     * @return PHPExcel
+     */
+    protected function getExcelDoc() {
+        return $this->_excel_doc;
+    }
+    
+    /**
+     * 
+     * @param PHPExcel $excel_doc
+     */
+    protected function setExcelDoc($excel_doc) {
+        $this->_excel_doc = $excel_doc;
+    }
+    
+    /**
+     * 
+     * @return PHPExcel_Worksheet
+     */
+    protected function getCurrentSheet() {
+        return $this->getExcelDoc()->getActiveSheet();
+    }
+    
+    /**
+     * 
+     * @return int
+     */
     protected function getCurrentLine() {
         return $this->_current_line;
     }
     
+    /**
+     * 
+     * @param int $current_line
+     */
     protected function setCurrentLine($current_line) {
         $this->_current_line = $current_line;
     }
     
+    /**
+     * 
+     * @return int
+     */
+    protected function goFirstLine() {
+        $this->setCurrentLine(self::FIRST_LINE);
+        return $this->getCurrentLine();
+    }
+    
+    /**
+     * 
+     * @param int $number_move nombre de ligne à avancer
+     * @return int retourne la ligne avant le déplacement
+     */
+    protected function moveCurrentLine($number_move=1){
+        $current_line = $this->getCurrentLine();
+        $this->setCurrentLine($this->getCurrentLine() + $number_move);
+        return $current_line;
+    }
+    
+    /**
+     * 
+     * @return Stage
+     */
     public function getStage() {
         return $this->_Stage;
     }
     
+    /**
+     * 
+     * @param Stage $stage
+     */
     public function setStage($stage) {
         if($stage)
         {
@@ -75,4 +157,13 @@ abstract class Document {
     protected function setDbAccess() {
         $this->_db_access = new DatabaseAccess();
     }
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getLink() {
+        return '<p></p>';
+    }
+    //</editor-fold>
 }
--- init.php
+++ init.php
@@ -3,6 +3,5 @@
     
     require_once 'model/config.class.php';
     require_once 'Autoloader.class.php';
-    
+    require_once 'phpExcel/PHPExcel.php';
     Autoloader::register();
-?>
