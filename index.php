e8b04b63b50cf24beea5b242d68ccd8b02a81def
Alexandre <jenicot.alexandre@gmail.com>
2015-03-25 10:41:12 +0100

    Ajout des modifications dans l'index pour les tests

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ use Exception;
 
     try {
         include('init.php');
-        GeneratorDocument::generateDocument(942,  GeneratorDocument::TYPE_MDS);
-        $excel = GeneratorDocument::getLinkDocument(942,  GeneratorDocument::TYPE_MDS);
+        GeneratorDocument::generateDocumentUnique(942,  GeneratorDocument::TYPE_MDS);
+        $excel = GeneratorDocument::getLinkDocumentUnique(942,  GeneratorDocument::TYPE_MDS);
     } catch (Exception $ex) {
         echo $ex->getMessage();
     }
@@ -20,12 +20,53 @@ use Exception;
     <body>
         <a href="<?php echo $excel; ?>">
             <img src="./images/excel.png" alt="lien vers document Excel"/>
-        <a/>
+        <a/><br/>
         <?php
+        //debug pour branche PerYear
         $DAO = new \stageOff\data\DatabaseAccess();
-        $tab_mat = $DAO->getMatriculeEtudiantPerYear(2015);
-        $tab_year = $DAO->getYearWithFinalStudent();
+        //debug Data Per year
+//        $tab_mat = $DAO->getMatriculeEtudiantPerYear(2013);
+        $tab_year = $DAO->getYearWithFinalStudent(data\DatabaseAccess::TYPE_EVALUATION_MDS);
         var_dump($tab_year);
-        ?>
+        
+        $etu = $DAO->getStage(942);
+        $sheet = new business\SheetExcelEtudiant($etu);
+//        $worksheet = $sheet->createSheet();
+//        $test = new \PHPExcel();
+//        $test->addSheet($worksheet);
+//        $writer = new \PHPExcel_Writer_Excel2007($test);
+//        var_dump($writer->getPHPExcel());
+//        $writer = new \PHPExcel_Writer_Excel2007($sheet->debugCreate());
+//        $writer->save(model\config::read('ROOT') . 'evaluation/test.xlsx');
+          
+//        creation fichier document unique
+//        $testfinal = new business\DocumentExcelUnique(business\DocumentExcelUnique::TYPE_QUESTIONNAIRE_MDS, 942);
+//        var_dump($testfinal->getFileName());
+//        var_dump($testfinal->getLink());
+//        $testfinal->generateDocument();
+        
+        $matricule_etudiant= 322364;
+        echo "id stage pour l'étudiant $matricule_etudiant";
+        $stages = $DAO->getAllIdStageForEtudiant($matricule_etudiant, data\DatabaseAccess::TYPE_EVALUATION_MDS);
+        var_dump($stages);
+        
+        $year = 2014;
+        echo "liste etudiant pour l'année $year - " . ($year+1);
+        $etudiants = $DAO->getMatriculeEtudiantPerYear($year, data\DatabaseAccess::TYPE_EVALUATION_MDS);
+        var_dump($etudiants);
+        $nb=0;
+        foreach ($etudiants as $etudiant)
+        {
+            $stages = $DAO->getAllIdStageForEtudiant($etudiant, data\DatabaseAccess::TYPE_EVALUATION_MDS);
+            var_dump($stages);
+            $nb += count($stages);
+        }
+        echo "$nb <br/>";
+        echo '_____________________________';
+        var_dump(GeneratorDocument::getYearWithFinalStudent(GeneratorDocument::TYPE_EVALUATION_MDS));
+?>
+        <a href="<?php echo GeneratorDocument::getDocumentPerYear($year, GeneratorDocument::TYPE_MDS); ?>">
+            <img src="./images/excel.png" alt="lien vers document Excel"/>
+        <a/><br/>
     </body>
 </html>
